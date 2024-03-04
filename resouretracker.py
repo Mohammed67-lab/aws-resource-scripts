f0a51784903014e12ae074de839f1d946ba39137
Mohammed Asif Raza <Mohammed-asif.raza@hp.com>
2024-03-04 01:29:49 +0000

    Add Python script for interacting with AWS services
    -- This commit adds a Python script that uses the Boto3 library to interact with the Amazon API.
    -- The script creates a session using AWS credentials and the Boto3 library, and then creates clients for the EC2, S3, and IAM services.
    -- It executes the describe-instances command to get information about available EC2 instances, the list-buckets command to get information about available S3 buckets, and the list-users command to get information about available IAM users.
    -- The script prints the information about available EC2 instances, S3 buckets, and IAM users to the console.
    -- This script is a useful tool for monitoring and managing AWS resources, and it can be easily extended to include additional functionality or to interact with other AWS services.

---
+++ resouretracker.py
@@ -0,0 +1,50 @@
+import boto3
+
+# Create a session using your AWS credentials
+session = boto3.Session(
+    aws_access_key_id='AKIAT3PACG4O5NMPTVP2',
+    aws_secret_access_key='zu6Nzyl+Ma+MagkLzWKms4uzhehc+WYrSuia5Xat',
+    region_name='ap-south-1'
+)
+
+# Create a client for the EC2 service
+ec2_client = session.client('ec2')
+
+# Execute the describe-instances command
+response = ec2_client.describe_instances()
+
+# Get Instance type and Instance id of available ec2 in the given region.
+for i, reservation in enumerate(response['Reservations']):
+    for instance in reservation["Instances"]:
+        instance_id = instance["InstanceId"]
+        instance_type = instance["InstanceType"]
+
+        print(f"The Available EC2 in AWS are \n{i+1}\t The instance_id = {instance_id}\t instance_type = {instance_type}")
+
+# Create a client for the s3 service        
+s3_client = session.client('s3')
+
+# Execute the S3 list-buckets command
+s3_response = s3_client.list_buckets()
+
+for i, bucket in enumerate(s3_response['Buckets']):
+    bucket_name = bucket['Name']
+    print(f"The Available buckets in AWS are \n{i + 1} \t {bucket_name}")
+
+
+# Create a client for iam service
+iam_client = session.client('iam')
+
+# Execute the list users in iam command
+iam_response = iam_client.list_users()
+
+
+print("The Iam Users available in AWS are")
+for i, user in enumerate(iam_response['Users']):
+    iam_user_name = user['UserName']
+    iam_user_id = user['UserId']
+    print(f"{i+1}\tUser-id is {iam_user_id} and name is {iam_user_name}")
+
+
+
+    
